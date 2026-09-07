5509872d066acddaf773b0deb8c4e3bfb5272ab0
oritri98 <ismatsiddiquee98@gmail.com>
2026-09-07 23:59:31 +0600

    Update gamepad.py controller initialization

--- gamepad.py
+++ gamepad.py
@@ -9,3 +9,10 @@ BAUD_RATE = 115200
 ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
 time.sleep(2) # Give the ESP32 a moment to reset
 
+# 2. Initialize Controller
+pygame.init()
+pygame.joystick.init()
+
+if pygame.joystick.get_count() == 0:
+    print("No controller found. Please plug it in!")
+    exit()
